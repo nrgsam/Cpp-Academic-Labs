b5e497b1041136901002e001b5d925fcdb4a08b3
AliGHanizade <mralighanizade@gmail.com>
2025-12-05 07:32:51 +0330

    Implement MaxNumberFunction to find the maximum number from user inputs

---
+++ src/01-MaxNumber/MaxNumber.cpp
@@ -0,0 +1,28 @@
+/*
+Get number of inputs from user and return the maximum number among them.
+Author: Ali Ghanizade
+Date: 1404-9-13
+*/
+
+#include <iostream>
+
+#include "../utils/GetNumber.cpp"
+
+using namespace std;
+
+void MaxNumberFunction() {
+    int max = 0;
+    cout << "Enter Count of numbers: ";
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++) {
+        int number = GetNumber();
+        if (number > max) {
+            max = number;
+        }
+    }
+    string messages = "Max Number Function Executed Successfully. Max Number is: " ;
+    cout << messages << max << endl;
+  
+   
+}
