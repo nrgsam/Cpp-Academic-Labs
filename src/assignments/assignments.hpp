e17a7027a91c8677dc30a6e1eed46f000c8d9101
nrgsam <nargeemami86@gmail.com>
2025-12-09 17:26:28 +0330

    Base Conversion of Decimal numbers to Binary

--- src/assignments/assignments.hpp
+++ src/assignments/assignments.hpp
@@ -2,6 +2,6 @@
 #include "01-MaxNumber/MaxNumber.hpp"
 #include "02-Divisibility/Divisibility.hpp"
 #include "03-Fibonacci/Fibonacci.hpp"
-
+#include "05-DecimalToBinary/DecimalToBinary.hpp"
 
 #include "04-Array/Array.hpp"
